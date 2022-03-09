1eabf7388ee9ceabdb99aea803d15f55d47d376c
Daniel.C <68897785+OkoroDanielC@users.noreply.github.com>
2022-03-09 16:36:37 +0100

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.10;
 
+contract HelloWorld {
+
+    event UpdatedMessages(string oldStr, string newStr);
+
+     string public message;
+
+constructor(string memory initMessage) {
+    
+     message = initMessage;
+   }
+   
+   function update(string memory newMessage) public {
+      string memory oldMsg = message;
+      message = newMessage;
+      emit UpdatedMessages(oldMsg, newMessage);
+   }
+
+}
